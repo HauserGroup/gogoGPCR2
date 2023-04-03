148785f736eb9a8f9457d8211f2478478c3c4916
jsture <jakobsturemadsen@gmail.com>
2023-04-03 13:38:29 +0200

    added liftOver and PHESANT scripts

---
+++ scripts/04_run_phesant.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+phenotype=$1
+trait=$2
+pheno_file_dir="/mnt/project/Data/phenotypes"
+
+run_phesant="mkdir -p /tmp/${phenotype}.phesant && \
+    Rscript WAS/phenomeScan.r --phenofile='${pheno_file_dir}/${phenotype}.${trait}.raw.tsv' --variablelistfile='variable-info/outcome-info.tsv' --datacodingfile='variable-info/data-coding-ordinal-info.txt' --resDir='/tmp/${phenotype}.phesant' --userId='xeid' --tab=TRUE  --save --skipirnt"
+        
+dx run swiss-army-knife -iin="/Data/dummy.file" \
+   -icmd="${run_phesant}" --tag="PHESANT" --instance-type "mem1_ssd1_v2_x16"\
+   --destination="/Data/phenotypes/${phenotype}.${trait}.phesant" \
+   -iimage="jsture/phesant2" --yes;
