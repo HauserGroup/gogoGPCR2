30d7bf53470669f193c72e46ae6b9f1c002c7e62
jsture <jakobsturemadsen@gmail.com>
2023-04-03 13:28:41 +0200

    updated Docker for PHESANT

--- docker/phesant/install_packages.R
+++ docker/phesant/install_packages.R
@@ -1,10 +1,13 @@
-install.packages("devtools")
+# library(devtools)
 
-library(devtools)
+install.packages("remotes",
+    repos = "http://cran.rstudio.com/",
+    dependencies = TRUE
+)
 
-install_version("optparse", version = "1.3.2")
-install_version("MASS", version = "7.3-45")
-install_version("data.table")
-install_version("lmtest", version = "0.9-34")
-install_version("nnet", version = "7.3-12")
-install_version("forestplot")
+remotes::install_version("optparse", version = "1.3.2")
+remotes::install_version("MASS", version = "7.3-45")
+remotes::install_version("data.table")
+remotes::install_version("lmtest", version = "0.9-34")
+remotes::install_version("nnet", version = "7.3-12")
+remotes::install_version("forestplot")
