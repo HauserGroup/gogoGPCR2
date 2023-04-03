a3289c59be2d0ea2c1e9f2cde19c8a511d583463
jsture <jakobsturemadsen@gmail.com>
2023-04-03 15:52:31 +0000

    updated run_phesant script

--- scripts/04_run_phesant.sh
+++ scripts/04_run_phesant.sh
@@ -4,8 +4,8 @@ phenotype=$1
 trait=$2
 pheno_file_dir="/mnt/project/Data/phenotypes"
 
-run_phesant="mkdir -p /tmp/${phenotype}.phesant && \
-    Rscript WAS/phenomeScan.r --phenofile='${pheno_file_dir}/${phenotype}.${trait}.raw.tsv' --variablelistfile='variable-info/outcome-info.tsv' --datacodingfile='variable-info/data-coding-ordinal-info.txt' --resDir='/tmp/${phenotype}.phesant' --userId='xeid' --tab=TRUE  --save --skipirnt"
+run_phesant="mkdir -p /tmp/${phenotype}.phesant && cd /PHESANT && \
+    Rscript WAS/phenomeScan.r --phenofile='${pheno_file_dir}/${phenotype}.${trait}.raw.tsv' --variablelistfile='../variable-info/outcome-info.tsv' --datacodingfile='../variable-info/data-coding-ordinal-info.txt' --resDir='/tmp/${phenotype}.phesant' --userId='xeid' --tab=TRUE --skipirnt=TRUE --save"
         
 dx run swiss-army-knife -iin="/Data/dummy.file" \
    -icmd="${run_phesant}" --tag="PHESANT" --instance-type "mem1_ssd1_v2_x16"\
