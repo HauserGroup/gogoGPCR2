df8b5b90392e62cadba2c6947cc86f7a1fb9b8c5
petergerlach1 <92981513+petergerlach1@users.noreply.github.com>
2023-05-11 11:24:38 +0200

    Update paths

--- scripts/2_regenie_step2_interaction_QT.sh
+++ scripts/2_regenie_step2_interaction_QT.sh
@@ -1,30 +1,35 @@
 #!/bin/sh
 
 TRAIT="QT"
-data_file_dir="/mnt/project/Data"
+data_file_dir="/mnt/project/Burden"
 
-prompt="Enter GENE and PHENOTYPE for Step 2 (GENE burden files, PHENOTYPE .tsv file, and PHENOTYPE.${TRAIT}.LOCO/FIRTH files must exist):   "
+prompt="Enter GENE and PHENOTYPE for Step 2 (GENE burden files, PHENOTYPE .tsv file, and PHENOTYPE.${TRAIT}.LOCO files must exist):   "
 read -p "$prompt" GENE PHENOTYPE 
 
-cp "${data_file_dir}/step1/${PHENOTYPE}.${TRAIT}.LOCO/${PHENOTYPE}.${TRAIT}.step1_firth.list" .
-cp "${data_file_dir}/step1/${PHENOTYPE}.${TRAIT}.LOCO/${PHENOTYPE}.${TRAIT}.step1_pred.list" .
+cp "${PHENOTYPE}.${TRAIT}.LOCO/${PHENOTYPE}.${TRAIT}.step1_pred.list" .
 
-sed -i "s|/home/dnanexus/out/out/|${data_file_dir}/step1/${PHENOTYPE}.${TRAIT}.LOCO/|" "${PHENOTYPE}.${TRAIT}.step1_pred.list" 
-sed -i "s|/home/dnanexus/out/out/|${data_file_dir}/step1/${PHENOTYPE}.${TRAIT}.LOCO/|" "${PHENOTYPE}.${TRAIT}.step1_firth.list"
+sed -i "s|/tmp/|${data_file_dir}/${PHENOTYPE}.${TRAIT}.LOCO/|" "${PHENOTYPE}.${TRAIT}.step1_pred.list" 
+
+#48 /tmp/metabolic.QT.step1_1.loco
+
 
 mkdir -p "${PHENOTYPE}.${TRAIT}.${GENE}"
 
 ./regenie \
   --step 2 \
   --interaction-snp	"chr18:60372043:C:T"\ 
-  --bgen "${data_file_dir}/burden/${GENE}.bgen" \
-  --sample "${data_file_dir}/burden/${GENE}.sample" \
-  --phenoFile "${data_file_dir}/phenotypes/${PHENOTYPE}.${TRAIT}.final.tsv" \
-  --covarFile "${data_file_dir}/phenotypes/covariates.tsv" \
+  --bgen "${data_file_dir}/${GENE}.bgen" \
+  --sample "${data_file_dir}/${GENE}.sample" \
+  --ref-first \ 
+  --phenoFile "${data_file_dir}/${PHENOTYPE}.${TRAIT}.final.tsv" \
+  --covarFile "${data_file_dir}/covariates.tsv" \
   --firth --approx \
-  --use-null-firth "${PHENOTYPE}.${TRAIT}.step1_firth.list" \
+  #--use-null-firth "${PHENOTYPE}.${TRAIT}.step1_firth.list" \
+  --pred "${PHENOTYPE}.${TRAIT}.step1_pred.list" \
   --bsize 200 \
+  --anno-file "${data_file_dir}/${PHENOTYPE}.annotations" \
+  --set-list "${data_file_dir}/${PHENOTYPE}.setlist" \
   --no-condtl \ 
   --force-condtl \
   --out "${PHENOTYPE}.${TRAIT}.${GENE}/${PHENOTYPE}.${TRAIT}.${GENE}.step2_interaction" \
-  --verbose # what is it? 
+  --verbose  
