8fd9dac57e41b8e6263bfb11cd997b109b5686e2
petergerlach1 <92981513+petergerlach1@users.noreply.github.com>
2023-05-11 17:15:49 +0200

    Update 2_regenie_step2_interaction_QT.sh
    cannot read   --pred "${PHENOTYPE}.${TRAIT}.step1_pred.list" \

--- scripts/2_regenie_step2_interaction_QT.sh
+++ scripts/2_regenie_step2_interaction_QT.sh
@@ -3,33 +3,48 @@
 TRAIT="QT"
 data_file_dir="/mnt/project/Burden"
 
-prompt="Enter GENE and PHENOTYPE for Step 2 (GENE burden files, PHENOTYPE .tsv file, and PHENOTYPE.${TRAIT}.LOCO files must exist):   "
-read -p "$prompt" GENE PHENOTYPE 
+#prompt="Enter PHENOTYPE for Step 2 (GENE burden files, PHENOTYPE .tsv file, and PHENOTYPE.${TRAIT}.LOCO files must exist):   "
+#read -p "$prompt" PHENOTYPE 
+PHENOTYPE="metabolic"
 
-cp "${PHENOTYPE}.${TRAIT}.LOCO/${PHENOTYPE}.${TRAIT}.step1_pred.list" .
+cp "${data_file_dir}/${PHENOTYPE}.${TRAIT}.LOCO/${PHENOTYPE}.${TRAIT}.step1_pred.list" .
 
-sed -i "s|/tmp/|${data_file_dir}/${PHENOTYPE}.${TRAIT}.LOCO/|" "${PHENOTYPE}.${TRAIT}.step1_pred.list" 
-
-#48 /tmp/metabolic.QT.step1_1.loco
+sed -i "s|/tmp/|${data_file_dir}/${PHENOTYPE}.${TRAIT}.LOCO/|g" "${PHENOTYPE}.${TRAIT}.step1_pred.list"
 
 
-mkdir -p "${PHENOTYPE}.${TRAIT}.${GENE}"
+mkdir -p "/${PHENOTYPE}.${TRAIT}"
 
-./regenie \
+
+run_regenie_step2="
+regenie \
   --step 2 \
-  --interaction-snp	"chr18:60372043:C:T"\ 
-  --bgen "${data_file_dir}/${GENE}.bgen" \
-  --sample "${data_file_dir}/${GENE}.sample" \
-  --ref-first \ 
+  --interaction-snp "chr18:60372043:C:T" \
+  --bgen "${data_file_dir}/${PHENOTYPE}.bgen" \
+  --sample "${data_file_dir}/${PHENOTYPE}.sample" \
+  --ref-first \
   --phenoFile "${data_file_dir}/${PHENOTYPE}.${TRAIT}.final.tsv" \
   --covarFile "${data_file_dir}/covariates.tsv" \
-  --firth --approx \
-  #--use-null-firth "${PHENOTYPE}.${TRAIT}.step1_firth.list" \
   --pred "${PHENOTYPE}.${TRAIT}.step1_pred.list" \
   --bsize 200 \
-  --anno-file "${data_file_dir}/${PHENOTYPE}.annotations" \
   --set-list "${data_file_dir}/${PHENOTYPE}.setlist" \
-  --no-condtl \ 
+  --no-condtl \
   --force-condtl \
-  --out "${PHENOTYPE}.${TRAIT}.${GENE}/${PHENOTYPE}.${TRAIT}.${GENE}.step2_interaction" \
-  --verbose  
+  --out ${PHENOTYPE}.${TRAIT}.step2_interaction2 \
+  --verbose
+"
+
+#   --use-null-firth "${PHENOTYPE}.${TRAIT}.step1_firth.list" 
+#   --firth --approx \
+#   --anno-file "${data_file_dir}/${PHENOTYPE}.annotations" \
+
+
+#dx run swiss-army-knife -iin="/${PHENOTYPE}.${TRAIT}.LOCO/${PHENOTYPE}.${TRAIT}.step1_pred.list" \
+#   -icmd="${run_regenie_step2}" \
+#   --tag="Step2" --instance-type "mem1_hdd1_v2_x16" \ 
+#   -iimage="jsture/phesant2" \
+#   --destination="/Burden" --brief --yes;
+
+dx run swiss-army-knife \
+   -icmd="${run_regenie_step2}" --tag="Step2" --instance-type "mem1_ssd1_v2_x16"\
+   --destination="/Burden" \
+   -iimage="ghcr.io/rgcgithub/regenie/regenie:v3.2.6.gz" --yes;
