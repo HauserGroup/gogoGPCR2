e65b2760903e3c278db5c31d7d712e9649b1db28
jsture <jakobsturemadsen@gmail.com>
2024-09-24 09:38:04 +0200

    scripts for het only for some reason

---
+++ scripts/WES/project_PG/step2_qt_inter_d470n.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+# Does not work as regenie removes interacting SNP from the analysis
+TRAIT="QT"
+data_file_dir="/mnt/project/GLP2R"
+PHENOTYPE="metabolic"
+GENE="GLP2R"
+TEST="dominant"
+mkdir -p "${PHENOTYPE}.${TRAIT}.${TEST}.D470N"
+
+run_regenie_step2="regenie \
+  --step 2 \
+  --qt \
+  --test ${TEST} \
+  --bgen ${data_file_dir}/metabolic.bgen \
+  --sample ${data_file_dir}/metabolic.sample \
+  --ref-first \
+  --extract ${data_file_dir}/d470n.keep.txt \
+  --phenoFile ${data_file_dir}/metabolic.QT.final.tsv \
+  --covarFile ${data_file_dir}/covariates.tsv \
+  --pred ${data_file_dir}/metabolic.QT.d470n.step1_pred.list \
+  --interaction-snp chr17:9889451:G:A \
+  --bsize 200 \
+  --out ${PHENOTYPE}.${TRAIT}.${TEST}.D470N \
+  --verbose"
+
+dx run swiss-army-knife \
+   -icmd="${run_regenie_step2}" --tag="Step2" --instance-type "mem1_ssd1_v2_x16"\
+   --destination="/GLP2R" \
+   -iimage="ghcr.io/rgcgithub/regenie/regenie:v3.6.gz" --yes --priority "high"
