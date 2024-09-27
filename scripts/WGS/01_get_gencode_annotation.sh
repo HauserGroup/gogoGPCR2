e7529a3a5f4a5822eb07014b0b52b179fc3c76b8
Javier Sanchez Lorente <67375457+javiersanchezlorente@users.noreply.github.com>
2024-09-27 08:00:20 +0200

    Upload script for gencode annotations
    Script to download, unzip and upload gencode annotation files

---
+++ scripts/WGS/01_get_gencode_annotation.sh
@@ -0,0 +1,3 @@
+wget https://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_46/gencode.v46.annotation.gtf.gz
+gunzip gunzip gencode.v46.annotation.gtf.gz
+dx upload gencode.v46.annotation.gtf --path /WGS_Javier/WGS_QC/
